f2c7ea168df433777da338df23740054873b1abb
Kayce Basques <kayce@google.com>
2023-12-27 17:36:40 -0800

    Build for Discovery board (building but emulation not working)

--- gn/src/app.cc
+++ gn/src/app.cc
@@ -1,11 +1,14 @@
-#include <unistd.h>
+#include <stdio.h>
 
 #include "pw_log/log.h"
 
+// Maybe?
+// https://github.com/fcayci/stm32f4-bare-metal/blob/master/projects/uart/uart.c
+
 int main() {
     while (true) {
-        PW_LOG_INFO("Hello, world!");
-	sleep(3);
+        PW_LOG_INFO("Hello, PW_LOG_INFO!");
+        printf("Hello, printf!");
     }
     return 0;
 }
