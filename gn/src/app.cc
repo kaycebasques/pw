831407649e6f0bccff9baaaa2694d38133760f0d
Kayce Basques <kayce@google.com>
2023-12-28 12:54:30 -0800

    Add STM32CUBE lib (not working)

--- gn/src/app.cc
+++ gn/src/app.cc
@@ -1,14 +1,12 @@
-#include <stdio.h>
-
 #include "pw_log/log.h"
-
-// Maybe?
-// https://github.com/fcayci/stm32f4-bare-metal/blob/master/projects/uart/uart.c
 
 int main() {
     while (true) {
         PW_LOG_INFO("Hello, PW_LOG_INFO!");
         printf("Hello, printf!");
+        for (int i = 0; i < 1000000; i++) {
+            // Wait...
+        }
     }
     return 0;
 }
