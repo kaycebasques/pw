bca1c340650736e85883e9dbfb02bc29304a51c4
Kayce Basques <kayce@google.com>
2024-01-05 11:22:00 -0800

    Use pw_function in hello_world_bare

--- src/hello_world_bare.cc
+++ src/hello_world_bare.cc
@@ -2,10 +2,17 @@
 
 #include "pw_log/log.h"
 #include "pw_string/string_builder.h"
+#include "pw_function/function.h"
 
-int main() {
+void print_message() {
     pw::StringBuffer<32> message;
     message << "Hello, world!";
     PW_LOG_INFO("%s", message.c_str());
+    return;
+}
+
+int main() {
+    pw::Function<void()> hello_world(print_message);
+    hello_world();
     return 0;
 }
