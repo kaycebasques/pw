b9112d0edb5f068d231488efe14a2f1fae30fc1b
Kayce Basques <kayce@google.com>
2024-01-04 18:40:32 -0800

    Use pw_string

--- src/hello_world_bare.cc
+++ src/hello_world_bare.cc
@@ -1,8 +1,11 @@
 #define PW_LOG_MODULE_NAME "app"
 
 #include "pw_log/log.h"
+#include "pw_string/string_builder.h"
 
 int main() {
-    PW_LOG_INFO("Hello, world!");
+    pw::StringBuffer<32> message;
+    message << "Hello, world!";
+    PW_LOG_INFO("%s", message.c_str());
     return 0;
 }
--- src/hello_world_sys.cc
+++ src/hello_world_sys.cc
@@ -1,9 +1,12 @@
 #define PW_LOG_MODULE_NAME "app"
 
 #include "pw_log/log.h"
+#include "pw_string/string_builder.h"
 
 namespace pw::system {
     void UserAppInit() {
-        PW_LOG_INFO("Hello, world!");
+        pw::StringBuffer<32> message;
+        message << "Hello, world!";
+        PW_LOG_INFO("%s", message.c_str());
     }
 }
