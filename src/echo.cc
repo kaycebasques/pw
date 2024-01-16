cf6318602b3c1f2e7ca0a2e5d652ececae84fcb4
Kayce Basques <kayce@google.com>
2024-01-16 12:59:31 -0800

    Save incomplete stuff

--- src/echo.cc
+++ src/echo.cc
@@ -1,13 +1,15 @@
 #include <cstddef>
 
-#include "pw_sys_io/sys_io.h"
+// #// include "pw_sys_io/sys_io.h"
+#include "pw_sys_io_stm32cube/init.h"
 
 int main() {
+    pw_sys_io_Init();
     while (true) {
-        std::byte data;
-        pw::sys_io::ReadByte(&data).IgnoreError();
-        std::byte mychar = (std::byte) 'c';
-        pw::sys_io::WriteByte(mychar).IgnoreError();
+        // std::byte data;
+        // pw::sys_io::ReadByte(&data).IgnoreError();
+        // std::byte mychar = (std::byte) 'c';
+        // pw::sys_io::WriteByte(mychar).IgnoreError();
     }
     return 0;
 }
