432ea4ce3f7d3a53e7f4be4198c21d6b0ba942ef
Kayce Basques <kayce@google.com>
2024-01-24 11:16:35 -0800

    Update pw_function example

--- src/pw_function.cc
+++ src/pw_function.cc
@@ -1,42 +1,42 @@
-// Copyright 2023 The Pigweed Authors
-//
-// Licensed under the Apache License, Version 2.0 (the "License"); you may not
-// use this file except in compliance with the License. You may obtain a copy of
-// the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-// License for the specific language governing permissions and limitations under
-// the License.
-
 #include <cstddef>
 #include <string_view>
 
 #include "pw_sys_io/sys_io.h"
 #include "pw_function/function.h"
 
-void a_impl(void) {
-    const std::string_view message = "handling a...";
-    pw::sys_io::WriteLine(message).IgnoreError();
+// void handle_user_command(int arg, void (*callback)(int result));
+
+// Return an int and do stuff based on the retval
+int _impl(std::byte command) {
+    std::byte response;
+    switch (command) {
+        case static_cast<std::byte>('a'):
+            response = (std::byte) 'a';
+            pw::sys_io::WriteByte(response).IgnoreError();
+            return 0;
+        case static_cast<std::byte>('x'):
+            response = (std::byte) '!';
+            pw::sys_io::WriteByte(response).IgnoreError();
+            return 1;
+    }
+    return 0;
 }
 
-pw::Function<void(void)> a_callback(a_impl);
+const pw::Function<int(std::byte)> callback(_impl);
+
+// void handle_user_command(int arg, void (*callback)(int result));
+void handle_user_command(std::byte command, const pw::Function<int(std::byte)>& callback) {
+    int result = callback(command);
+    if (result) {
+        // Do something...
+    }
+}
 
 int main() {
     std::byte data;
     while (true) {
         pw::sys_io::ReadByte(&data).IgnoreError();
-        switch (data) {
-            case static_cast<std::byte>('a'):
-                a_callback();
-                // Will crash on the second invocation because the callback is
-                // assigned to a null pointer here.
-                a_callback = nullptr;
-                break;
-        }
+        handle_user_command(data, callback);
     }
     return 0;
 }
